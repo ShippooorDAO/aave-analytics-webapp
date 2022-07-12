8f406737b1b1396bd5efbb1134f868a8fc3e5eee
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2022-07-11 22:59:13 -0400

    Fix breadcrumb rendering

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -9,11 +9,12 @@ export default function Breadcrumbs({ links }: { links: BreadcrumbLink[] }) {
   return (
     <div>
       {links.map(({ title, uri }, i) => (
-        <Link href={uri} key={i}>
-          <a>
-            {title} {i !== links.length - 1 ? '> ' : ''}
-          </a>
-        </Link>
+        <>
+          <Link href={uri} key={i}>
+            <a>{title}</a>
+          </Link>
+          {i !== links.length - 1 ? ' > ' : ''}
+        </>
       ))}
     </div>
   );
