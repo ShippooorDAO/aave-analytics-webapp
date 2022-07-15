fcca4eda419bba442573f30221b33d9505e48a32
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2022-07-15 14:03:53 -0400

    Fix Breadcrumbs console warning

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -9,12 +9,12 @@ export default function Breadcrumbs({ links }: { links: BreadcrumbLink[] }) {
   return (
     <div>
       {links.map(({ title, uri }, i) => (
-        <>
-          <Link href={uri} key={i}>
+        <span key={i}>
+          <Link href={uri}>
             <a>{title}</a>
           </Link>
           {i !== links.length - 1 ? ' > ' : ''}
-        </>
+        </span>
       ))}
     </div>
   );
