bfa4e4699a78ac43724740ae0baa11bcd7f4ef8c
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2022-07-21 09:59:52 -0400

    Fix accounts caching

--- src/shared/AaveAnalyticsApi/AaveAnalyticsApiCacheConfig.ts
+++ src/shared/AaveAnalyticsApi/AaveAnalyticsApiCacheConfig.ts
@@ -11,7 +11,7 @@ export const cacheConfig = {
             '$sortDirection',
             '$filter',
             '$search',
-            '$simulatedPriceOracles',
+            '$simulatedTokenPrices',
           ],
           merge(
             existing: AccountsGraphQLSchemaType | undefined,
